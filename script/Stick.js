b7851afe19bde48af57f6730ba544cf2a12b63f8
Mridul Gupta <mridul.gupta@gmail.com>
2018-06-19 10:45:14 +0530

    Stick rotation with mouse

--- script/Stick.js
+++ script/Stick.js
@@ -3,22 +3,28 @@ const STICK_ORIGIN = new Vector2(970, 11);
 function Stick(position) {
 
   this.position = position;
+  this.rotation = 0;
 
 }
 
 
 Stick.prototype.update = function() {
 
-  // // Testing
-  // this.position = Mouse.position;
-  //
-  // if (Mouse.left.pressed)
-  //   console.log("Left pressed");
+  this.updateRotation();
 }
 
 
 Stick.prototype.draw = function() {
 
-  Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN);
+  Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN, this.rotation);
 
+}
+
+
+Stick.prototype.updateRotation = function() {
+
+  let opposite = Mouse.position.y - this.position.y;
+  let adjucent = Mouse.position.x - this.position.x;
+
+  this.rotation = Math.atan2(opposite, adjucent);
 }
