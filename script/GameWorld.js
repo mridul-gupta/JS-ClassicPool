e4ab7020f8266ed6f0888eda38ed24fd85f8114a
Mridul Gupta <mridul.gupta@gmail.com>
2018-06-29 13:42:44 +0530

    Added all balls

--- script/GameWorld.js
+++ script/GameWorld.js
@@ -4,7 +4,26 @@ const DELTA = 1/100;
 
 function GameWorld() {
 
-  this.whiteBall = new Ball(new Vector2(411, 413), COLOR.WHITE);
+  this.balls = [
+    [new Vector2(1022, 413),COLOR.YELLOW], //1
+    [new Vector2(1056, 393),COLOR.YELLOW], //2
+    [new Vector2(1056, 433),COLOR.RED], //3
+    [new Vector2(1090, 374),COLOR.RED], //4
+    [new Vector2(1090, 413),COLOR.BLACK], //5
+    [new Vector2(1090, 452),COLOR.YELLOW], //6
+    [new Vector2(1126, 354),COLOR.YELLOW], //7
+    [new Vector2(1126, 393),COLOR.RED], //8
+    [new Vector2(1126, 433),COLOR.YELLOW], //9
+    [new Vector2(1126, 472),COLOR.RED], //10
+    [new Vector2(1162, 335),COLOR.RED], //11
+    [new Vector2(1162, 374),COLOR.RED], //12
+    [new Vector2(1162, 413),COLOR.YELLOW], //13
+    [new Vector2(1162, 452),COLOR.RED], //14
+    [new Vector2(1162, 491),COLOR.YELLOW], //15
+    [new Vector2(411, 413),COLOR.WHITE] //WHITE
+  ].map(params => new Ball(params[0], params[1]));
+
+  this.whiteBall = this.balls[this.balls.length - 1];
   this.stick = new Stick(
     new Vector2(411, 413),
     this.whiteBall.shoot.bind(this.whiteBall));
@@ -14,9 +33,12 @@ function GameWorld() {
 GameWorld.prototype.update = function() {
 
   this.stick.update();
-  this.whiteBall.update();
 
-  if (!this.BallMoving() && this.stick.shot) {
+  for(i=0; i<this.balls.length; i++) {
+    this.balls[i].update(DELTA);
+  }
+
+  if (!this.ballsMoving() && this.stick.shot) {
     this.stick.reposition (this.whiteBall.position);
   }
 
@@ -26,11 +48,23 @@ GameWorld.prototype.draw = function() {
 
   Canvas.drawImage(sprites.background, {x:0, y:0});
 
+  for(i=0; i<this.balls.length; i++) {
+    this.balls[i].draw();
+  }
   this.stick.draw();
-  this.whiteBall.draw();
+
 }
 
 
-GameWorld.prototype.BallMoving = function() {
-  return this.whiteBall.moving;
+GameWorld.prototype.ballsMoving = function() {
+  let ballsMoving = false;
+
+  for(i=0; i<this.balls.length; i++) {
+    if (this.balls[i].moving) {
+      ballsMoving = true;
+      break;
+    }
+  }
+
+  return ballsMoving;
 }
